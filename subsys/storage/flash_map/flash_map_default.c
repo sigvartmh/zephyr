b3b4eb2cc219a8566c2238aa33eb38d235f679fc
Håkon Øye Amundsen <haakon.amundsen@nordicsemi.no>
2019-02-14 13:54:27 +0000

    hello world booting
    Signed-off-by: Håkon Øye Amundsen <haakon.amundsen@nordicsemi.no>

--- subsys/storage/flash_map/flash_map_default.c
+++ subsys/storage/flash_map/flash_map_default.c
@@ -7,28 +7,37 @@
 
 #include <zephyr.h>
 #include <flash_map.h>
-
-#define FLASH_AREA_FOO(i, _)				\
-	{.fa_id = i,					\
-	 .fa_off = DT_FLASH_AREA_##i##_OFFSET,		\
-	 .fa_dev_name = DT_FLASH_AREA_##i##_DEV,	\
-	 .fa_size = DT_FLASH_AREA_##i##_SIZE,},
-
 #include <pm_config.h>
-#define SLOT_SIZE (PM_CFG_APP_SIZE / 2)
-#define FLASH_AREA_IMAGE_0_OFFSET (PM_CFG_APP_ADDRESS)
-#define FLASH_AREA_IMAGE_0_SIZE SLOT_SIZE
-#define FLASH_AREA_IMAGE_1_OFFSET FLASH_AREA_IMAGE_0_OFFSET + FLASH_AREA_IMAGE_0_SIZE
-#define FLASH_AREA_IMAGE_1_SIZE FLASH_AREA_IMAGE_0_SIZE
-#define FLASH_AREA_IMAGE_SCRATCH_OFFSET PM_CFG_MCUBOOT_SCRATCH_ADDRESS
-#define FLASH_AREA_IMAGE_SCRATCH_SIZE PM_CFG_MCUBOOT_SCRATCH_SIZE
-#define FLASH_AREA_IMAGE_STORAGE_OFFSET PM_CFG_MCUBOOT_STORAGE_ADDRESS
-#define FLASH_AREA_IMAGE_STORAGE_SIZE PM_CFG_MCUBOOT_STORAGE_SIZE
-#define FLASH_AREA_MCUBOOT_OFFSET PM_CFG_MCUBOOT_ADDRESS
-#define FLASH_AREA_MCUBOOT_SIZE PM_CFG_MCUBOOT_SIZE
+
+#define IMAGE_SIZE ((PM_CFG_APP_SIZE - PM_CFG_MCUBOOT_SCRATCH_SIZE - PM_CFG_MCUBOOT_STORAGE_SIZE)/2)
 
 const struct flash_area default_flash_map[] = {
-	UTIL_LISTIFY(DT_FLASH_AREA_NUM, FLASH_AREA_FOO, ~)
+	{
+		.fa_id = 0,
+		.fa_off = PM_CFG_MCUBOOT_ADDRESS,
+		.fa_dev_name = DT_FLASH_AREA_0_DEV,
+		.fa_size = PM_CFG_MCUBOOT_SIZE,
+	},{
+		.fa_id = 1,
+		.fa_off = PM_CFG_APP_ADDRESS,
+		.fa_dev_name = DT_FLASH_AREA_0_DEV,
+		.fa_size = IMAGE_SIZE,
+	},{
+		.fa_id = 2,
+		.fa_off = PM_CFG_APP_ADDRESS + IMAGE_SIZE,
+		.fa_dev_name = DT_FLASH_AREA_0_DEV,
+		.fa_size = IMAGE_SIZE,
+	},{
+		.fa_id = 3,
+		.fa_off = PM_CFG_MCUBOOT_SCRATCH_ADDRESS,
+		.fa_dev_name = DT_FLASH_AREA_0_DEV,
+		.fa_size = PM_CFG_MCUBOOT_SCRATCH_SIZE,
+	},{
+		.fa_id = 4,
+		.fa_off = PM_CFG_MCUBOOT_STORAGE_ADDRESS,
+		.fa_dev_name = DT_FLASH_AREA_0_DEV,
+		.fa_size = PM_CFG_MCUBOOT_STORAGE_SIZE,
+	},
 };
 
 const int flash_map_entries = ARRAY_SIZE(default_flash_map);
